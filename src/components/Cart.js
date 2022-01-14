7f89a68756a467e5d674f8ad986aea1b2a44bf53
Muhammad Anis <anisiqbal484@gmail.com>
2022-01-14 20:42:37 +0500

    header done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import pic from '../assets/img/pic 1.jpg';
 import '../assets/css/cart.css';
 
 function Cart(props) {
      const [products, setProduct] = useState([
-          { Id: 1, image: 'https:images.app.goo.gl/24X454JZfSB8f7Gj9', name: 'Power Movie', price: 1000, quantity: '6', totalprice: ' 60000', },
+          { Id: 1, image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQIr7E4HUjXEOaRAS_RP6-770L8dvL6MlB6dF-g1wd4anLqkj3M', name: 'Power Movie', price: 1000, quantity: '6', totalprice: ' 60000', },
+          { Id: 1, image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQIr7E4HUjXEOaRAS_RP6-770L8dvL6MlB6dF-g1wd4anLqkj3M', name: 'Power Movie', price: 1000, quantity: '6', totalprice: ' 60000', },
+          { Id: 1, image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQIr7E4HUjXEOaRAS_RP6-770L8dvL6MlB6dF-g1wd4anLqkj3M', name: 'Power Movie', price: 1000, quantity: '6', totalprice: ' 60000', },
+          { Id: 1, image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQIr7E4HUjXEOaRAS_RP6-770L8dvL6MlB6dF-g1wd4anLqkj3M', name: 'Power Movie', price: 1000, quantity: '6', totalprice: ' 60000', },
      ]);
 
      return (
@@ -14,21 +17,20 @@ function Cart(props) {
                     products.map((item, i) => {
                          return (
                               <div className="row">
-                                   <div className="col-2">
-                                        <img src={item.image} class= "w-50"/>
-                                   </div>
+                                  
                                    <div className="col-4">
                                         <h4>{item.name}</h4>
                                    </div>
                                    <div className="col-2">
-                                        price
+                                        <p>{item.price}</p>
                                    </div>
                                    <div className="col-1">
-                                        Qty
+                                        <p>{item.quantity}</p>
                                    </div>
                                    <div className="col-2">
-                                        total price
+                                        <p>{item.totalprice}</p>
                                    </div>
+
                                    <div className="col-1">
                                         <button type="button" class="close" aria-label="Close">
                                              <span aria-hidden="true">&times;</span>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,46 +1,34 @@
 import '../assets/css/header.css';
 import logo from '../assets/img/logo.png'
 
-import{
-    Link
-}from "react-router-dom";
+import{ Link }from "react-router-dom";
 
 function Header(){
     return(
-        <nav class="navbar navbar-dark bg-primary">
-            <div className="container">
-                <div className="row w-100">
-                    <div className="col-md-3">
-                        <a className="logo">
-                            <img src={logo}/>
-                        </a>
-                    </div>
-                    <div  className="col-md-9">
-                        <ul>
-                            <li>
-                                <Link to="/home">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/About">About</Link>
-                            </li>
-                            <li>
-                               <Link to="/Contact">Contact</Link>
-                            </li>
-                            <li>
-                               <Link to="/Cart">Cart</Link>
-                            </li>
-                        </ul>
+        <div>
+            <div className="header">
+                <div className="container">
+                    <div className="row">
 
-                    </div>
-                    <div>
+                        <div className="col-3">
+                            <a className="logo">
+                                <img src={logo}/>
+                            </a>
+                        </div>
 
+                        <div  className="col-9">
+                            <ul>
+                                <li><Link to="/home">Home</Link></li>
+                                <li><Link to="/About">About</Link></li>
+                                <li><Link to="/Contact">Contact</Link></li>
+                                <li><Link to="/Cart">Cart</Link></li>
+                            </ul>
+                        </div>
                     </div>
 
                 </div>
-
             </div>
-            
-        </nav>
+        </div>
     );
 }
 
